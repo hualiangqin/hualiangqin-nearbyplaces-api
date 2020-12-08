4a66e60de7e8acef26704ec42bdda0140e2f01a9
Hualiang Qin <46637518+hualiangqin@users.noreply.github.com>
2020-12-08 16:13:56 -0700

    update

---
+++ data.js
@@ -0,0 +1,82 @@
+let places =
+[
+    {
+        id: 1,
+        name: "place1",
+        city: "tucson",
+        state: "AZ",
+        type : "restaurant",
+        reviews:[
+            {
+                stars: 5,
+                comment: "nice place"        
+            },
+            {
+                stars: 2,
+                comment: "will not come again"
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "place 3",
+        city: "tucson",
+        state: "AZ",
+        type: "shopping mall",
+        reviews:[
+            {
+                stars: 1,
+                comment: "too many people"
+            },
+            {
+                stars: 4,
+                comment: "have bought what I wanted"
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "place 8",
+        city: "tempe",
+        state: "AZ",
+        type: "dry cleaning",
+        reviews: [
+            {
+                stars: 0,
+                comment: "every dirty"
+            }
+        ]
+    },
+    {
+        id: 4,
+        name: "place 28",
+        city: "phoenix",
+        state: "AZ",
+        type: "barber",
+        reviews: [
+            {
+                stars: 5,
+                comment: "nice person!"
+            }
+        ]
+    },
+    {
+        id: 5,
+        name: "place 167",
+        city: "apache junction",
+        state: "AZ",
+        type: "grocery",
+        reviews :[
+            {
+                stars: 4,
+                comment: "have everything I needed"
+            },
+            {
+                stars: 1,
+                comment: "the vege is not fresh"
+            }
+        ]
+    }
+]
+
+module.exports.places = places;
--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
 var cors = require('cors');
+const { res, req, response } = require('express'); 
+const bodyParser = require('body-parser');
+let data = require('./data');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3002;
 
 //middlewares
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to nearbyplaces API');
+});
+
+app.get('/places', (req, res) => {
+    let places = data.places;
+    let metadata = places.map(x => {
+        return {id: x.id, name: x.name, city: x.city, state: x.state, type: x.type};
+    });
+    res.json(metadata);
+});
+
+app.get('/search/:searchTerm/:location', (req, res) =>{
+    let searchTerm = request.params.searchTerm;
+    let location = request.params.location;
+
+});
+
+app.post('/place', (req, res) => {
+    let place = request.body;
+
+    response.json({message: 'The place is saved successfully'})
+});
+
+app.post('/review/:placeId', (req, res) => {
+    let placeId = request.params.placeId;
+    let review = request.body;
+
+    response.json({message: `The review for place=${placeId} succeeded.`});
 });
 
 app.listen(port, ()=> {
